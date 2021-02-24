4a3ec81798489d6e8243f10eb124e04718e5db4d
Matt Dawson <mattdawson18@gmail.com>
2021-02-24 12:45:48 +0000

    added tests and categories feature, created association between categories and articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,9 @@ class ApplicationController < ActionController::Base
      @current_user ||= User.find(session[:user_id]) if session[:user_id] 
     end
     
-     def logged_in?
-    !!current_user 
-  end
+    def logged_in?
+     !!current_user 
+    end
   
   def require_user
    if !logged_in?
